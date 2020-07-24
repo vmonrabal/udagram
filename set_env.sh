fc8761da75c787338418ee06342dafe5a905b7a3
Vicente Monrabal <vmonrabal@gmail.com>
2020-07-24 20:05:41 +0200

    Adding set_env file

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=
+export JWT_SECRET=
+export URL=
